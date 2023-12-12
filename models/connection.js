8520745b625bdd34d390323cd7715020d6f577a5
Roger <roger.nguyen@hotmail.fr>
2023-12-12 17:10:34 +0100

    connection file added

---
+++ models/connection.js
@@ -0,0 +1,25 @@
+const mysql = require("mysql");
+
+module.exports = {
+  connect: async () => {
+    const config = {
+      host: process.env.DB_HOST,
+      user: process.env.DB_USER,
+      password: process.env.DB_PASSWORD,
+      database: process.env.DB_DATABASE,
+    };
+
+    const connection = await mysql.createConnection(config);
+    connection.connect();
+
+    connection.on("connect", () => {
+      console.log("Connexion à la base de données réussie");
+    });
+
+    connection.on("error", (err) => {
+      console.error("Erreur de connexion à la base de données", err);
+    });
+
+    return connection;
+  },
+};
